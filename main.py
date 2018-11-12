314498ad672214653760bade749bf82a71952c0a
Naoki Yokoyama <naoki.yokoyama7@gmail.com>
2018-11-12 10:54:04 -0500

    separated mta functions into its own file

--- main.py
+++ main.py
@@ -1,27 +1,15 @@
-from google.transit import gtfs_realtime_pb2
-import requests
+from utils import mta
+
 import time # imports module for Epoch/GMT time conversion
 import os # imports package for dotenv
-import dotenv
 import yaml
-import csv
-import urllib2
-import operator
 from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
-from protobuf_to_dict import protobuf_to_dict
+
+collected_times = []
+
+TRAINS_TO_ID = yaml.load(open('trains_to_id.yaml'))
 
 app = Flask(__name__)
-
-dotenv.load_dotenv('api_key.env') # loads .env from root directory
-
-# The root directory requires a .env file with API_KEY assigned/defined within
-# and dotenv installed from pypi. Get API key from http://datamine.mta.info/user
-api_key = os.environ['API_KEY']
-trains_to_id = yaml.load(open('trains_to_id.yaml'))
-stations_url = 'http://web.mta.info/developers/data/nyct/subway/Stations.csv'
-# Because the data feed includes multiple arrival times for a given station
-# a global list needs to be created to collect the various times
-collected_times = []
 
 def timeUntil(arrival_time):
     current_time = int(time.time())
@@ -30,111 +18,34 @@ def timeUntil(arrival_time):
 
 @app.route('/')
 def main():
-    return 'This is the main page'
+    return 'This is the main page. You may want to visit /line-select'
 
 @app.route('/line-select')
 def selectLine():
-    # if request.method == 'POST':
-    #     line = request.form['line']
-    #     train_id = trains_to_id[line]
-    #     print 'TRAIN LINE IS: ' + line + '\n'
-    #     print 'TRAIN ID IS: ' +str(train_id)
-    #     return redirect('/')
-    # else:
-    return render_template('line.html', lines=trains_to_id)
+    return render_template('line.html', lines=TRAINS_TO_ID)
 
 @app.route('/<line>/station-select', methods = ['GET','POST'])
 def selectStation(line):
     global collected_times
     if request.method == 'GET':
-        # CSV file reader
-        stations_response = urllib2.urlopen(stations_url)
-        stations_csv = csv.reader(stations_response)
-        next(stations_csv) #Skips the first line in the csv file because it's the header.
-        station_names = []
-
-        for row in stations_csv:
-            if line in (row[7]): # row[7] = Lines passing through station
-                station_names.append(row[5])   # row[5] = Station name
-
+        station_names = mta.get_station_names_from_line(line)
         return render_template('stationSelect.html', stations = station_names)
 
-    else:
+    elif request.method == 'POST':
         direction = request.form['direction']
         station = request.form['station']
+        print 'direction',direction
+        print 'station',station
+        print 'line',line
+        collected_times = mta.get_sorted_times_from_station(direction, station, line)
+        print 'updated master list', collected_times
 
-        stations_response = urllib2.urlopen(stations_url)
-        stations_csv = csv.reader(stations_response)
-        next(stations_csv) #Skips the first line in the csv file because it's the header.
-
-        gtfs_id = ''
-
-
-        for row in stations_csv:
-            if line in (row[7]) and station == row[5]:
-                gtfs_id = row[2]
-
-        if direction == 'uptown':
-            stop_id = gtfs_id + 'N'
-        else:
-            stop_id = gtfs_id + 'S'
-
-        print 'Line:'+line
-        print 'Station: '+station
-        print 'gtfs_id is: '+gtfs_id
-        print 'stop_id: '+stop_id
-
-        train_id = trains_to_id[line]
-        # Requests subway status data feed from City of New York MTA API
-        response = requests.get('http://datamine.mta.info/mta_esi.php?key={}&feed_id={}'.format(api_key,train_id))
-        feed = gtfs_realtime_pb2.FeedMessage()
-        feed.ParseFromString(response.content)
-        #print feed #Uncomment to actually look at the feed you're accessing
-
-        # The MTA data feed uses the General Transit Feed Specification (GTFS) which
-        # is based upon Google's "protocol buffer" data format. While possible to
-        # manipulate this data natively in python, it is far easier to use the
-        # "pip install --upgrade gtfs-realtime-bindings" library which can be found on pypi
-
-        subway_feed = protobuf_to_dict(feed) # subway_feed is a dictionary
-        realtime_data = subway_feed['entity'] # train_data is a list
-
-        # This function takes a converted MTA data feed and a specific station ID and
-        # loops through various nested dictionaries and lists to (1) filter out active
-        # trains, (2) search for the given station ID, and (3) append the arrival time
-        # of any instance of the station ID to the collected_times list
-        def station_time_lookup(train_data, stop, collected_times):
-            print 'Clearing collected_times...'
-            collected_times = []
-            for trains in train_data: # trains are dictionaries
-                if trains.get('trip_update', False) != False:
-                    unique_train_schedule = trains['trip_update'] # train_schedule is a dictionary with trip and stop_time_update
-                    trip_info = unique_train_schedule['trip'] #trip_info is a list of the train info that going through the stops
-                    route_id = trip_info['route_id']
-                    unique_arrival_times = unique_train_schedule['stop_time_update'] # arrival_times is a list of arrivals
-                    for scheduled_arrivals in unique_arrival_times: #arrivals are dictionaries with time data and stop_ids
-                        if scheduled_arrivals.get('stop_id', False) == stop:
-                            time_data = scheduled_arrivals['arrival']
-                            unique_time = time_data['time']
-                            if unique_time != None:
-                                route_time = (route_id, unique_time)
-                                print 'appropriate time found!'
-                                collected_times.append(route_time)
-                                print collected_times
-            return collected_times
-        # Run the above function for the station ID for Broadway-Lafayette
-        collected_times = station_time_lookup(realtime_data, stop_id, collected_times)
-        print 'collected_times master list'
-        print collected_times
-
-        # Sort the collected times list in chronological order (the times from the data
-        # feed are in Epoch time format)
-        collected_times.sort(key=operator.itemgetter(1))
         return redirect(url_for('timesDisplay'))
 
 @app.route('/times-display')
 def timesDisplay():
     global collected_times
+
     print '[display]: collected_times master list'
     print collected_times
     # Pop off the earliest  arrival times from the list
---
+++ utils/mta.py
@@ -0,0 +1,96 @@
+import yaml
+import dotenv
+import os # imports package for dotenv
+from google.transit import gtfs_realtime_pb2
+import urllib2
+import csv
+import requests
+import operator
+from protobuf_to_dict import protobuf_to_dict
+
+dotenv.load_dotenv('api_key.env') # loads .env from root directory
+
+# The root directory requires a .env file with API_KEY assigned/defined within
+# and dotenv installed from pypi. Get API key from http://datamine.mta.info/user
+api_key = os.environ['API_KEY']
+TRAINS_TO_ID = yaml.load(open('trains_to_id.yaml'))
+stations_url = 'http://web.mta.info/developers/data/nyct/subway/Stations.csv'
+# Because the data feed includes multiple arrival times for a given station
+# a global list needs to be created to collect the various times
+collected_times = []
+
+def get_stations_csv():
+    # CSV file reader
+    stations_response = urllib2.urlopen(stations_url)
+    stations_csv = csv.reader(stations_response)
+    # Skips the first line in the csv file because it's the header.
+    next(stations_csv)
+    return stations_csv
+
+def get_station_names_from_line(line):
+    stations_csv = get_stations_csv()
+    station_names = [row[5] for row in stations_csv if line in row[7]] 
+    return station_names
+
+def get_sorted_times_from_station(direction, station, line):
+    stations_csv = get_stations_csv()
+    gtfs_id = ''
+    for row in stations_csv:
+        if line in (row[7]) and station == row[5]:
+            gtfs_id = row[2]
+
+    if direction == 'uptown':
+        stop_id = gtfs_id + 'N'
+    else:
+        stop_id = gtfs_id + 'S'
+
+    print 'Line:'+line
+    print 'Station: '+station
+    print 'gtfs_id is: '+gtfs_id
+    print 'stop_id: '+stop_id
+
+    train_id = TRAINS_TO_ID[line]
+    # Requests subway status data feed from City of New York MTA API
+    response = requests.get('http://datamine.mta.info/mta_esi.php?key={}&feed_id={}'.format(api_key,train_id))
+    feed = gtfs_realtime_pb2.FeedMessage()
+    feed.ParseFromString(response.content)
+    #print feed #Uncomment to actually look at the feed you're accessing
+
+    # The MTA data feed uses the General Transit Feed Specification (GTFS) which
+    # is based upon Google's "protocol buffer" data format. While possible to
+    # manipulate this data natively in python, it is far easier to use the
+    # "pip install --upgrade gtfs-realtime-bindings" library which can be found on pypi
+
+    subway_feed = protobuf_to_dict(feed) # subway_feed is a dictionary
+    realtime_data = subway_feed['entity'] # train_data is a list
+
+    # Run the above function for the station ID for Broadway-Lafayette
+    collected_times = station_time_lookup(realtime_data, stop_id)
+
+    # Sort the collected times list in chronological order (the times from the data
+    # feed are in Epoch time format)
+    collected_times.sort(key=operator.itemgetter(1))
+    return collected_times
+
+# This function takes a converted MTA data feed and a specific station ID and
+# loops through various nested dictionaries and lists to (1) filter out active
+# trains, (2) search for the given station ID, and (3) append the arrival time
+# of any instance of the station ID to the collected_times list
+def station_time_lookup(train_data, stop):
+    collected_times = []
+    for trains in train_data: # trains are dictionaries
+        if trains.get('trip_update', False) != False:
+            unique_train_schedule = trains['trip_update'] # train_schedule is a dictionary with trip and stop_time_update
+            trip_info = unique_train_schedule['trip'] #trip_info is a list of the train info that going through the stops
+            route_id = trip_info['route_id']
+            unique_arrival_times = unique_train_schedule['stop_time_update'] # arrival_times is a list of arrivals
+            for scheduled_arrivals in unique_arrival_times: #arrivals are dictionaries with time data and stop_ids
+                if scheduled_arrivals.get('stop_id', False) == stop:
+                    time_data = scheduled_arrivals['arrival']
+                    unique_time = time_data['time']
+                    if unique_time != None:
+                        route_time = (route_id, unique_time)
+                        collected_times.append(route_time)
+                        print 'route time added:', route_time
+                        
+    return collected_times
