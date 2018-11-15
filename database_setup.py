a64f2ec6d4b22f3edf8ce5786748889fa8b6b6d3
Will <william.guiracoche@gmail.com>
2018-11-15 15:59:01 -0500

    feat: Create database to store users and their favorite stations

---
+++ database_setup.py
@@ -0,0 +1,36 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+
+class SavedStations(Base):
+    __tablename__ = 'restaurant'
+
+    gtfs_id = Column(Integer, primary_key=True)
+    order = Column(Integer)
+    user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'gtfs_id': self.name,
+            'user_id': self.user_id,
+            'order': self.order,
+        }
+
+engine = create_engine('sqlite:///savedstations.db')
+Base.metadata.create_all(engine)
