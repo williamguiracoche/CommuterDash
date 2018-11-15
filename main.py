a7ba7b50d4e2d8728fbedc2ebba8529d39a706e2
Will <william.guiracoche@gmail.com>
2018-11-15 16:23:01 -0500

    feat: Create user helper functions

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ import requests
 
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Restaurant, MenuItem, User
+from database_setup import Base, User, SavedStations
 
 app = Flask(__name__)
 
@@ -125,6 +125,25 @@ def gconnect():
     print "done!"
     return output
 
+# User Helper Functions
+def createUser(login_session):
+    newUser = User(name=login_session['username'], email=login_session[
+                   'email'], picture=login_session['picture'])
+    session.add(newUser)
+    session.commit()
+    user = session.query(User).filter_by(email=login_session['email']).one()
+    return user.id
+
+def getUserInfo(user_id):
+    user = session.query(User).filter_by(id=user_id).one()
+    return user
+
+def getUserID(email):
+    try:
+        user = session.query(User).filter_by(email=email).one()
+        return user.id
+    except:
+        return None
 
 # DISCONNECT - Revoke a current user's token and reset their login_session
 @app.route('/gdisconnect')
