bc3fedac71eea32327511e2f192b5a467815674e
vagrant <vagrant@vagrant.vm>
2018-11-16 06:26:34 +0000

    feat: Add station gtfs_id to user in database if user is logged in

--- main.py
+++ main.py
@@ -205,6 +205,16 @@ def selectStation(line):
         station_name = mta.get_station_name_from_gtfs_id(gtfs_id)
         print station_name
         collected_times = mta.get_sorted_times_from_station(direction, station_name, line)
+
+        if 'username' in login_session:
+            username = login_session['username']
+            user_id = login_session['user_id']
+            print user_id
+            newStation = SavedStation(gtfs_id = gtfs_id, user_id=login_session['user_id'])
+            session.add(newStation)
+            flash('Hey %s, we have added %s station to your favorites!' % (username, station_name))
+            print('Hey %s, we have added %s station to your favorites!' % (username, station_name))
+            session.commit()
         return redirect(url_for('timesDisplay'))
 
 @app.route('/times-display')
