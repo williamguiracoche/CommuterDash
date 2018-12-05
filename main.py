f6dc0a1f76d113049d2403adb968405b5aa54cb6
Will <william.guiracoche@gmail.com>
2018-12-05 03:21:32 -0500

    fix: Get client_secrets.json from config var if there isn't a file provided

--- main.py
+++ main.py
@@ -65,7 +65,10 @@ def gconnect():
 
     try:
         # Upgrade the authorization code into a credentials object
-        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        try:
+            oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        except InvalidClientSecretsError:
+            oauth_flow = flow_from_clientsecrets(json.parse(os.environ['CLIENT_SECRETS']), scope='')
         oauth_flow.redirect_uri = 'postmessage'
         credentials = oauth_flow.step2_exchange(code)
     except FlowExchangeError:
